d443cbed5ae50eefdf65c69316ff9eb6e78267b8
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2025-03-11 19:19:10 +0530

    HCNAGES --J

--- src/Common/headers/nav/Body/index.jsx
+++ src/Common/headers/nav/Body/index.jsx
@@ -103,9 +103,9 @@ export default function Body({
   const handleMobileAboutUsClick = () => {
     // Check if popup is already open, then close it
     if (hoverPopup) {
-      setHoverPopup(false);  // Close the popup
+      setHoverPopup(false); // Close the popup
     } else {
-      setHoverPopup(true);   // Open the popup
+      setHoverPopup(true); // Open the popup
       hoverPopupHandler("About us"); // Trigger the hoverPopupHandler for "About us"
     }
   };
@@ -182,7 +182,7 @@ export default function Body({
               className="about-us-mobile-popup"
               onClick={handleMobileAboutUsClick}
             >
-              <svg
+              {/* <svg
                 clip-rule="evenodd"
                 fill-rule="evenodd"
                 stroke-linejoin="round"
@@ -192,6 +192,16 @@ export default function Body({
                 className="MobileSvg"
               >
                 <path d="m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z" />
+              </svg> */}
+              <svg
+                width="24"
+                height="24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                className="MobileSvg"
+              >
+                <path d="M0 3l12 18 12-18h-24zm12 16.197l-10.132-15.197h20.263l-10.131 15.197" />
               </svg>
               {/* <Image
                 src={nav_logo} // You can replace this with your actual About Us SVG path
